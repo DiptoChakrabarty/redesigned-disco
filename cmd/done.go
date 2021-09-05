95ed005aa789a62b39f3b33516cce176b879957f
DiptoChakrabarty <diptochuck123@gmail.com>
2021-09-05 19:13:51 +0530

    list and delete tasks working

--- cmd/done.go
+++ cmd/done.go
@@ -3,6 +3,8 @@ package cmd
 import (
 	"fmt"
 	"github.com/spf13/cobra"
+	"go-scheduler/database"
+	"os"
 	"strconv"
 )
 
@@ -20,6 +22,25 @@ func DoneCmdImplement(cmd *cobra.Command, args []string) {
 			fmt.Println("Failed to parse given argument", arg)
 		} else {
 			ids = append(ids, id)
+		}
+	}
+	tasks, err := database.GetAllTasks()
+	if err != nil {
+		fmt.Println("Some Error Occured ", err.Error())
+		os.Exit(1)
+	}
+
+	for _, id := range ids {
+		if id <= 0 || id > len(tasks) {
+			fmt.Println("InValid Task Number")
+			continue
+		}
+		task := tasks[id-1]
+		err := database.DeleteTasks(task.Key)
+		if err != nil {
+			fmt.Printf("Falied to complete task \"%d Error %s\n", id, err)
+		} else {
+			fmt.Printf("Complete task \"%d", id)
 		}
 	}
 	fmt.Println(ids)
